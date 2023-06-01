b17442737392c2d7b32900e4425a172e81d8992b
ajit-mali <113754999+ajit-mali@users.noreply.github.com>
2023-06-01 02:38:23 +0000

    added jigsaw edited about-us and our_games created credit and cooming soon edited navigation bar 01062230809

--- games/jigsaw puzzle/puzzle.js
+++ games/jigsaw puzzle/puzzle.js
@@ -12,10 +12,24 @@ window.onload = function(){
         for (let c = 0; c < columns; c++) {
             //<img>
             let tile = document.createElement("img");
+<<<<<<< HEAD
             tile.src = "./images/blank2.jpg";
 
             document.getElementById("board").append(tile);
 
+=======
+            tile.src = "./images/blank.jpg";
+
+        //drag function
+        tile.addEventListener("dragstart", dragStart);
+        tile.addEventListener("dragover", dragOver);
+        tile.addEventListener("dragenter", dragEnter);
+        tile.addEventListener("dragleave", dragLeave);
+        tile.addEventListener("drop", dragDrop);
+        tile.addEventListener("dragend", dragEnd);
+    
+        document.getElementById("board").append(tile);
+>>>>>>> 77d1917 (added jigsaw edited about-us and our_games)
         }
     }
 
@@ -24,11 +38,71 @@ window.onload = function(){
     for (let i = 1; i <= rows*columns; i++){
         pieces.push(i.toString()); 
     }
+<<<<<<< HEAD
+=======
+    pieces.reverse();
+    for(let i = 0; i< pieces.length; i++){
+        let j = Math.floor(Math.random() * pieces.length)
+
+        //swap
+        let temp = pieces[i];
+        pieces[i] = pieces[j];
+        pieces[j] = temp;
+    }
+>>>>>>> 77d1917 (added jigsaw edited about-us and our_games)
 
     for (let i = 0; i < pieces.length; i++) {
         let tile = document.createElement("img");
         tile.src = "./images/" + pieces[i] + ".jpg";
+<<<<<<< HEAD
         document.getElementById("pieces").append(tile);
 
     }   
+=======
+
+        //drag function
+        tile.addEventListener("dragstart", dragStart);
+        tile.addEventListener("dragover", dragOver);
+        tile.addEventListener("dragenter", dragEnter);
+        tile.addEventListener("dragleave", dragLeave);
+        tile.addEventListener("drop", dragDrop);
+        tile.addEventListener("dragend", dragEnd);
+        
+        document.getElementById("pieces").append(tile);
+    }   
+}
+
+//drag tiles
+function dragStart() {
+    currTile = this; 
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this;
+}
+
+function dragEnd() {
+    if (currTile.src.includes("blank")) {
+        return;
+    }
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+    currTile.src = otherImg;
+    otherTile.src = currImg;
+
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
+>>>>>>> 77d1917 (added jigsaw edited about-us and our_games)
 }
--- webpages/about_us/about-us.js
+++ webpages/about_us/about-us.js
@@ -3,4 +3,6 @@ const main = document.querySelector('main');
 setTimeout(() => {
     main.style.backdropFilter = 'blur(5px)';
     console.log(vdo, main)
-}, 9000);
+}, 4500);
+
+vdo.playbackRate = 2;
